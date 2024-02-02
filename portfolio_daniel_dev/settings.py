26feaddd9dcad38b5ad2b6a2576228e73299ec84
Daniel Alcazar <dev.daniel.alcazar@gmail.com>
2024-02-01 20:16:52 -0600

    se modifica netlify linea 2 y settings en paths

--- portfolio_daniel_dev/settings.py
+++ portfolio_daniel_dev/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio_daniel_developer/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio_daniel_dev/portfolio_daniel_developer/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'portfolio_daniel_dev/staticfiles')
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio_daniel_developer/static')]
 
